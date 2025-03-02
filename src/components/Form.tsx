2c37d6c8fb1b564c0e12a6f39600855beb1c7522
Ignacia Silva <ignaciasilva1@gmail.com>
2025-03-02 07:00:18 -0300

    add id library and show components of user information

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,4 +1,5 @@
 import { useState, ChangeEvent, FormEvent, Dispatch } from "react";
+import {v4 as uuid4} from 'uuid'
 import { categories } from "../data/categories";
 import type { Activity } from "../types";
 import { ActivityActions } from "../reducers/activity-reducer";
@@ -7,7 +8,8 @@ type FormProps = {
   dispatch: Dispatch<ActivityActions>;
 };
 
-const initialState = {
+const initialState: Activity = {
+  id: uuid4(),
   category: 1,
   name: "",
   calories: 0
@@ -36,7 +38,9 @@ export default function Form({ dispatch }: FormProps) {
     e.preventDefault();
 
     dispatch({ type: "save-activity", payload: { newActivity: activity } });
-    setActivity(initialState)
+    setActivity({
+      ...initialState,
+    id: uuid4()})
   };
 
   return (
